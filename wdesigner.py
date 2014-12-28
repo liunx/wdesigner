24d2881ac548eb687221fe8181e9953a19e7ca04
Liunx <liunx163@163.com>
2014-12-28 23:03:30 +0800

    add genhtml function.

--- wdesigner.py
+++ wdesigner.py
@@ -5,25 +5,43 @@ import argparse
 import traceback
 import xml.etree.ElementTree as ET
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--input", help = "xml file")
-parser.add_argument("-o", "--output", help = "output file")
-args = parser.parse_args()
-if not args.input:
-    parser.print_help()
-    quit(1)
+tags = {
+        'property' : True,
+        'packing' : True,
+        'placeholder' : True,
+        }
 
-if not args.output:
-    parser.print_help()
-    quit(1)
+def genhtml(element):
+    if element.tag == 'object':
+        print(element.attrib)
+        print(element.attrib['id'])
+        for child in element.findall('child'):
+            genhtml(child)
+        print(element.attrib['id'])
+    elif tags.has_key(element.tag):
+        pass
+    else:
+        for child in element:
+            genhtml(child)
 
-try:
-    tree = ET.parse(args.input)
-except:
-    traceback.print_exc()
-    quit(1)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help = "xml file")
+    parser.add_argument("-o", "--output", help = "output file")
+    args = parser.parse_args()
+    if not args.input:
+        parser.print_help()
+        quit(1)
 
-root = tree.getroot()
-for child in root:
-    print(child.tag, child.attrib)
+    if not args.output:
+        parser.print_help()
+        quit(1)
 
+    try:
+        tree = ET.parse(args.input)
+    except:
+        traceback.print_exc()
+        quit(1)
+
+    root = tree.getroot()
+    genhtml(root)
